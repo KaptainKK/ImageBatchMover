12ac10c604c658b9eee33e91c0fff7d31ea8e35e
KaptainKK <156919503+KaptainKK@users.noreply.github.com>
2024-05-27 09:37:36 +0900

    Add files via upload

---
+++ ImageBatchMover/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace ImageBatchMover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFolderPath = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFolderPathSave = new System.Windows.Forms.TextBox();
+            this.buttonBrowseSave = new System.Windows.Forms.Button();
+            this.labelHowManyFiles = new System.Windows.Forms.Label();
+            this.textBoxHowManyFiles = new System.Windows.Forms.TextBox();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxFolderPath
+            // 
+            this.textBoxFolderPath.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxFolderPath.Location = new System.Drawing.Point(23, 47);
+            this.textBoxFolderPath.Name = "textBoxFolderPath";
+            this.textBoxFolderPath.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxFolderPath.Size = new System.Drawing.Size(446, 26);
+            this.textBoxFolderPath.TabIndex = 0;
+            this.textBoxFolderPath.TextChanged += new System.EventHandler(this.texBoxFolderPath_TextChanged);
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonBrowse.Location = new System.Drawing.Point(493, 47);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(148, 26);
+            this.buttonBrowse.TabIndex = 1;
+            this.buttonBrowse.Text = "フォルダを選択";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(19, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ソースフォルダ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(19, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "保存フォルダ （空フォルダのみ）";
+            // 
+            // textBoxFolderPathSave
+            // 
+            this.textBoxFolderPathSave.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxFolderPathSave.Location = new System.Drawing.Point(23, 121);
+            this.textBoxFolderPathSave.Name = "textBoxFolderPathSave";
+            this.textBoxFolderPathSave.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxFolderPathSave.Size = new System.Drawing.Size(446, 26);
+            this.textBoxFolderPathSave.TabIndex = 4;
+            this.textBoxFolderPathSave.TextChanged += new System.EventHandler(this.textBoxFolderPathSave_TextChanged);
+            // 
+            // buttonBrowseSave
+            // 
+            this.buttonBrowseSave.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonBrowseSave.Location = new System.Drawing.Point(493, 121);
+            this.buttonBrowseSave.Name = "buttonBrowseSave";
+            this.buttonBrowseSave.Size = new System.Drawing.Size(148, 26);
+            this.buttonBrowseSave.TabIndex = 5;
+            this.buttonBrowseSave.Text = "フォルダを選択";
+            this.buttonBrowseSave.UseVisualStyleBackColor = true;
+            this.buttonBrowseSave.Click += new System.EventHandler(this.buttonBrowseSave_Click);
+            // 
+            // labelHowManyFiles
+            // 
+            this.labelHowManyFiles.AutoSize = true;
+            this.labelHowManyFiles.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelHowManyFiles.Location = new System.Drawing.Point(19, 182);
+            this.labelHowManyFiles.Name = "labelHowManyFiles";
+            this.labelHowManyFiles.Size = new System.Drawing.Size(255, 19);
+            this.labelHowManyFiles.TabIndex = 6;
+            this.labelHowManyFiles.Text = "何枚ごとにサブフォルダに分けるか";
+            // 
+            // textBoxHowManyFiles
+            // 
+            this.textBoxHowManyFiles.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxHowManyFiles.Location = new System.Drawing.Point(23, 204);
+            this.textBoxHowManyFiles.Name = "textBoxHowManyFiles";
+            this.textBoxHowManyFiles.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxHowManyFiles.Size = new System.Drawing.Size(251, 26);
+            this.textBoxHowManyFiles.TabIndex = 7;
+            this.textBoxHowManyFiles.TextChanged += new System.EventHandler(this.textBoxHowManyFiles_TextChanged);
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Enabled = false;
+            this.ButtonStart.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ButtonStart.Location = new System.Drawing.Point(23, 264);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(161, 61);
+            this.ButtonStart.TabIndex = 8;
+            this.ButtonStart.Text = "Start";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(294, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(404, 144);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "注意\r\n・ソースフォルダ及びそのすべてのサブフォルダから、すべての画像\r\n  ファイル（jpg、png、bmp、gifのみ）が移動\r\n\r\n・3行目で指定した数に基" +
+    "づいて、画像ファイルが新しく生成され\r\n  るサブフォルダに分散\r\n\r\n・画像ファイルの移動が完了した後、ソースフォルダからは削除\r\n  ただし、対象外のファイ" +
+    "ルやフォルダはその場に残る　";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(117, 383);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(443, 31);
+            this.progressBar1.TabIndex = 10;
+            this.progressBar1.Visible = false;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Cursor = System.Windows.Forms.Cursors.HSplit;
+            this.labelStatus.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelStatus.Location = new System.Drawing.Point(273, 361);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(248, 19);
+            this.labelStatus.TabIndex = 11;
+            this.labelStatus.Text = "　　　　　　　   　　　　　　　　　　\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 435);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.textBoxHowManyFiles);
+            this.Controls.Add(this.labelHowManyFiles);
+            this.Controls.Add(this.buttonBrowseSave);
+            this.Controls.Add(this.textBoxFolderPathSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBoxFolderPath);
+            this.Name = "Form1";
+            this.Text = "FileOrganizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFolderPath;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFolderPathSave;
+        private System.Windows.Forms.Button buttonBrowseSave;
+        private System.Windows.Forms.Label labelHowManyFiles;
+        private System.Windows.Forms.TextBox textBoxHowManyFiles;
+        private System.Windows.Forms.Button ButtonStart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ ImageBatchMover/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+//TODO 応答なし、進捗メーター
+
+namespace ImageBatchMover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // ドラッグアンドドロップのイベントハンドラを設定
+            this.textBoxFolderPath.AllowDrop = true;
+            this.textBoxFolderPath.DragEnter += textBoxFolderPath_DragEnter;
+            this.textBoxFolderPath.DragDrop += textBoxFolderPath_DragDrop;
+
+            this.textBoxFolderPathSave.AllowDrop = true;
+            this.textBoxFolderPathSave.DragEnter += textBoxFolderPathSave_DragEnter;
+            this.textBoxFolderPathSave.DragDrop += textBoxFolderPathSave_DragDrop;
+
+            this.textBoxFolderPath.TextChanged += textBox_TextChanged;
+            this.textBoxFolderPathSave.TextChanged += textBox_TextChanged;
+            this.textBoxHowManyFiles.TextChanged += textBox_TextChanged;
+
+            labelStatus.TextAlign = ContentAlignment.MiddleCenter;
+            labelStatus.Text = "";
+        }
+
+        
+
+        private async void ButtonStart_Click(object sender, EventArgs e)
+        {
+            // Source and target folder paths from the text boxes
+            string sourceFolderPath = textBoxFolderPath.Text;
+            string baseTargetFolderPath = textBoxFolderPathSave.Text;
+
+            // Set the status message
+            labelStatus.Text = "ファイル確認中...";
+
+            // Wait a bit to allow the status message to be displayed
+            await Task.Delay(100);
+
+            // Check if the target folder is empty
+            if (Directory.GetFiles(baseTargetFolderPath).Length > 0 || Directory.GetDirectories(baseTargetFolderPath).Length > 0)
+            {
+                MessageBox.Show("Target folder must be empty.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+                return;
+            }
+
+            // File count limit from the text box
+            int fileCountLimit;
+            if (!int.TryParse(textBoxHowManyFiles.Text, out fileCountLimit))
+            {
+                // Show an error message if the input is not an integer
+                MessageBox.Show("Invalid input for file count. Please enter an integer.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Get all image files recursively from the source folder
+            string[] imageFiles = Directory.GetFiles(sourceFolderPath, "*", SearchOption.AllDirectories)
+                                          .Where(file => IsImageFile(file))
+                                          .ToArray();
+
+            // Reset the status message
+            labelStatus.Text = "";
+
+            // Counters for files and folders
+            int fileCount = 0;
+            int folderCount = 1;
+
+            // Create the first target folder
+            string targetFolderPath = Path.Combine(baseTargetFolderPath, $"Folder{folderCount}");
+            Directory.CreateDirectory(targetFolderPath);
+
+            // ProgressBarを表示
+            progressBar1.Visible = true;
+
+
+            // 進捗バーの最大値を設定
+            progressBar1.Maximum = imageFiles.Length;
+            progressBar1.Value = 0;
+
+            // 別スレッドでファイルの移動を行う
+            await Task.Run(() =>
+                {
+                    // Move the image files to the target folders
+                    foreach (string file in imageFiles)
+                    {
+                        // Move the file to the target folder
+                        string targetFilePath = Path.Combine(targetFolderPath, Path.GetFileName(file));
+                        File.Move(file, targetFilePath);
+
+                        // Increase the file count
+                        fileCount++;
+
+                        // If the file count has reached the limit, create a new folder
+                        if (fileCount % fileCountLimit == 0)
+                        {
+                            folderCount++;
+                            targetFolderPath = Path.Combine(baseTargetFolderPath, $"Folder{folderCount}");
+                            Directory.CreateDirectory(targetFolderPath);
+                        }
+
+                        // UI スレッドに進捗バーの更新を指示
+                        this.Invoke(new Action(() =>
+                        {
+                            progressBar1.Value++;
+                        }));
+                    }
+                });
+
+            // Display a message box when the operation is completed
+            MessageBox.Show("Images have been consolidated into multiple folders and original files deleted.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Application.Exit();
+        }
+
+        private bool IsImageFile(string filePath)
+        {
+            string extension = Path.GetExtension(filePath).ToLower();
+            return (extension == ".jpg" || extension == ".png" || extension == ".bmp" || extension == ".gif");
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBoxFolderPath.Text = folderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void buttonBrowseSave_Click(object sender, EventArgs e)
+        {
+            using (var folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBoxFolderPathSave.Text = folderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+
+        // ファイルやフォルダがテキストボックス上にドラッグされたときのイベントハンドラ
+        private void textBoxFolderPath_DragEnter(object sender, DragEventArgs e)
+        {
+            // ドラッグされたデータがファイルやフォルダである場合のみ、ドロップを許可する
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        // ファイルやフォルダがテキストボックスにドロップされたときのイベントハンドラ
+        private void textBoxFolderPath_DragDrop(object sender, DragEventArgs e)
+        {
+            // ドロップされたファイルやフォルダのパスを取得
+            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if (paths.Length > 0)
+            {
+                // 最初のファイルやフォルダのパスをテキストボックスに設定
+                textBoxFolderPath.Text = paths[0];
+            }
+        }
+
+        // Drag and drop event handlers for textBoxFolderPathSave
+        private void textBoxFolderPathSave_DragEnter(object sender, DragEventArgs e)
+        {
+            // Only allow a drop if a file or folder is being dragged
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void textBoxFolderPathSave_DragDrop(object sender, DragEventArgs e)
+        {
+            // Get the paths of the dropped files and folders
+            string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if (paths.Length > 0)
+            {
+                // Set the text box to the path of the first file or folder
+                textBoxFolderPathSave.Text = paths[0];
+            }
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            // Check if all textboxes have content and if textBoxHowManyFiles contains a valid integer
+            ButtonStart.Enabled = !string.IsNullOrWhiteSpace(textBoxFolderPath.Text)
+                && !string.IsNullOrWhiteSpace(textBoxFolderPathSave.Text)
+                && int.TryParse(textBoxHowManyFiles.Text, out _);
+        }
+
+        private void texBoxFolderPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxFolderPathSave_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxHowManyFiles_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
